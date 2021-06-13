952a14dde9dc1b835d06672e53d149d488a8b54d
Guillaume Roux <rouxguillaume8@gmail.com>
2021-06-13 12:36:03 +0200

    added golden unit tests

--- test/text_chunk_styling_test.dart
+++ test/text_chunk_styling_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:text_chunk_styling/text_chunk_styling.dart';
 
 void main() {
@@ -27,6 +29,55 @@ void main() {
           throwsAssertionError,
         );
       });
+    });
+  });
+
+  group('Styling', () {
+    testGoldens('display stylized text', (tester) async {
+      await loadAppFonts();
+      await tester.pumpWidget(
+        MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            body: Container(
+              child: TextChunkStyling(
+                text:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus erat purus, sollicitudin et velit sed, suscipit euismod ipsum. Etiam ultricies purus a nunc condimentum, sollicitudin mollis tortor maximus. Phasellus fringilla augue a leo molestie feugiat. Donec eget nisi vel metus rhoncus ultricies. Donec non semper mi. Suspendisse dictum orci molestie libero vehicula, ut faucibus massa luctus. Etiam sit amet urna tristique, ullamcorper ex at, feugiat ipsum. Mauris ut leo quis magna tempus euismod. Nam euismod mauris quam, quis iaculis ligula ornare quis. Nunc egestas urna ac mauris consequat, id tincidunt justo iaculis. Ut posuere risus elit, vel facilisis nulla lobortis non.',
+                highlightText: ['sum', 'ing', 'mod', 'ris', 'nam'],
+                highlightTextStyle:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+      );
+      await screenMatchesGolden(tester, 'style_1');
+    });
+
+    testGoldens('display multi-stylized text', (tester) async {
+      await loadAppFonts();
+      await tester.pumpWidget(
+        MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            body: Container(
+              child: TextChunkStyling(
+                text:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus erat purus, sollicitudin et velit sed, suscipit euismod ipsum. Etiam ultricies purus a nunc condimentum, sollicitudin mollis tortor maximus. Phasellus fringilla augue a leo molestie feugiat. Donec eget nisi vel metus rhoncus ultricies. Donec non semper mi. Suspendisse dictum orci molestie libero vehicula, ut faucibus massa luctus. Etiam sit amet urna tristique, ullamcorper ex at, feugiat ipsum. Mauris ut leo quis magna tempus euismod. Nam euismod mauris quam, quis iaculis ligula ornare quis. Nunc egestas urna ac mauris consequat, id tincidunt justo iaculis. Ut posuere risus elit, vel facilisis nulla lobortis non.',
+                highlightText: ['sum', 'ing', 'mod', 'ris', 'nam'],
+                multiTextStyles: [
+                  TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+                  TextStyle(color: Colors.amber, fontWeight: FontWeight.bold),
+                  TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+                  TextStyle(color: Colors.brown, fontWeight: FontWeight.bold),
+                  TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+      await screenMatchesGolden(tester, 'style_2');
     });
   });
 }
